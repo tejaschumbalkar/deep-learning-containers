b047fc681f42b411b44d6e08fbbb7f31246f890e
tejaschumbalkar <tejaschumbalkar@gmail.com>
2021-08-21 23:07:19 -0700

    enable sanity test

--- src/start_testbuilds.py
+++ src/start_testbuilds.py
@@ -75,6 +75,7 @@ def is_test_job_enabled(test_type):
     Check to see if a test job is enabled
     See if we should run the tests based on test types and config options.
     """
+    LOGGER.info(f"test_type {test_type}")
     if test_type == constants.SAGEMAKER_TESTS and config.is_sm_remote_test_enabled():
         return True
     if test_type == constants.EC2_TESTS and config.is_ec2_test_enabled():
@@ -86,7 +87,7 @@ def is_test_job_enabled(test_type):
             return True
         if test_type == constants.EKS_TESTS and config.is_eks_test_enabled():
             return True
-        if test_type == constants.SANITY_TESTS and config.is_eks_test_enabled():
+        if test_type == constants.SANITY_TESTS and config.is_sanity_test_enabled():
             return True
 
     return False
